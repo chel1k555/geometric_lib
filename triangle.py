c64f6c8acbfcaaed13f9d96e8bc6f30c7dd11014
chel1k_hohmas <popilev.sanya@gmail.com>
2024-12-20 23:27:11 +0300

    Update triangle.py

--- triangle.py
+++ triangle.py
@@ -1,6 +1,14 @@
+from math import sqrt
+
+
 def area(a, b, c):
-    return (a + b + c) / 2
+    if a < 0 or b < 0 or c < 0:
+        raise AssertionError("sides can't be negative")
+    x = (a + b + c) / 2
+    return sqrt(x * (x - a) * (x - b) * (x - c))
 
 
 def perimeter(a, b, c):
+    if a < 0 or b < 0 or c < 0:
+        raise AssertionError("sides can't be negative")
     return a + b + c
