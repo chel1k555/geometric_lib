a0fae01417c3cecbbe540a28057182105080a8da
chel1k_hohmas <popilev.sanya@gmail.com>
2024-12-21 13:09:04 +0300

    Update circle.py

--- circle.py
+++ circle.py
@@ -3,11 +3,11 @@ import math
 
 def area(r):
     if r < 0:
-        raise AssertionError("raduis can't be negative")
+        raise AssertionError("Радиус не может быть отрицательным")
     return math.pi * r * r
 
 
 def perimeter(r):
     if r < 0:
-        raise AssertionError("raduis can't be negative")
+        raise AssertionError("Радиус не может быть отрицательным")
     return 2 * math.pi * r
