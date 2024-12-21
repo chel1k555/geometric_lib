d30e9caeb7f59557a7799627ad39142adb5af803
chel1k_hohmas <popilev.sanya@gmail.com>
2024-12-21 13:24:58 +0300

    Update triangle.py

--- triangle.py
+++ triangle.py
@@ -1,14 +1,24 @@
-from math import sqrt
+import math
 
 
 def area(a, b, c):
-    if a < 0 or b < 0 or c < 0:
-        raise AssertionError("Стороны не могут быть отрицательными")
-    x = (a + b + c) / 2
-    return sqrt(x * (x - a) * (x - b) * (x - c))
+    if (
+        a <= 0 or b <= 0 or c <= 0
+        or not all(isinstance(side, (int, float)) for side in [a, b, c])
+        or (a + b <= c) or (a + c <= b) or (b + c <= a)
+    ):
+        raise ValueError("Sides must be positive numbers"
+                         " and form a valid triangle")
+    s = (a + b + c) / 2
+    return math.sqrt(s * (s - a) * (s - b) * (s - c))
 
 
 def perimeter(a, b, c):
-    if a < 0 or b < 0 or c < 0:
-        raise AssertionError("Стороны не могут быть отрицательными")
+    if (
+        a <= 0 or b <= 0 or c <= 0
+        or not all(isinstance(side, (int, float)) for side in [a, b, c])
+        or (a + b <= c) or (a + c <= b) or (b + c <= a)
+    ):
+        raise ValueError("Sides must be positive numbers"
+                         " and form a valid triangle")
     return a + b + c
