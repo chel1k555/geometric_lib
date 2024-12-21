f4a454a137868196005e98d261c9e9d5d0e00005
chel1k_hohmas <popilev.sanya@gmail.com>
2024-12-21 13:23:06 +0300

    Update test_circle.py

--- all_tests/test_circle.py
+++ all_tests/test_circle.py
@@ -1,38 +1,44 @@
 import unittest
-from math import pi
-from circle import area, perimeter
+import circle
+from scenarios import scenarios
 
 
-class TestCircle(unittest.TestCase):
+class TestCircleArea(unittest.TestCase):
     def test_area(self):
-        radius = 1
-        res = area(radius)
-        self.assertEqual(res, pi)
+        for radius, expected in scenarios['circle']['area']:
+            with self.subTest(radius=radius, expected=expected):
+                self.assertAlmostEqual(circle.area(radius), expected)
 
+    def test_area_invalid_negative(self):
+        for invalid_radius in [-1, -10]:
+            with self.subTest(radius=invalid_radius):
+                with self.assertRaises(ValueError):
+                    circle.area(invalid_radius)
+
+    def test_area_invalid_non_numeric(self):
+        for invalid_radius in ['a', None]:
+            with self.subTest(radius=invalid_radius):
+                with self.assertRaises(TypeError):
+                    circle.area(invalid_radius)
+
+
+class TestCirclePerimeter(unittest.TestCase):
     def test_perimeter(self):
-        radius = 1
-        res = perimeter(radius)
-        self.assertEqual(res, 2 * pi)
+        for radius, expected in scenarios['circle']['perimeter']:
+            with self.subTest(radius=radius, expected=expected):
+                self.assertAlmostEqual(circle.perimeter(radius), expected)
 
-    def test_area_zero(self):
-        radius = 0
-        res = area(radius)
-        self.assertEqual(res, 0)
+    def test_perimeter_invalid_negative(self):
+        for invalid_radius in [-1, -10]:
+            with self.subTest(radius=invalid_radius):
+                with self.assertRaises(ValueError):
+                    circle.perimeter(invalid_radius)
 
-    def test_perimeter_zero(self):
-        radius = 0
-        res = perimeter(radius)
-        self.assertEqual(res, 0)
-
-    def test_area_neg(self):
-        radius = -1
-        with self.assertRaises(AssertionError):
-            area(radius)
-
-    def test_perimeter_neg(self):
-        radius = -1
-        with self.assertRaises(AssertionError):
-            perimeter(radius)
+    def test_perimeter_invalid_non_numeric(self):
+        for invalid_radius in ['a', None]:
+            with self.subTest(radius=invalid_radius):
+                with self.assertRaises(TypeError):
+                    circle.perimeter(invalid_radius)
 
 
 if __name__ == '__main__':
