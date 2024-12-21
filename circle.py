7cc1564860480fb9b1946bbb421f540bd2a3ea0a
chel1k_hohmas <popilev.sanya@gmail.com>
2024-12-21 13:24:24 +0300

    Update circle.py

--- circle.py
+++ circle.py
@@ -1,13 +1,14 @@
+
 import math
 
 
 def area(r):
-    if r < 0:
-        raise AssertionError("Радиус не может быть отрицательным")
+    if r < 0 or not isinstance(r, (int, float)):
+        raise ValueError("Radius must be a non-negative number")
     return math.pi * r * r
 
 
 def perimeter(r):
-    if r < 0:
-        raise AssertionError("Радиус не может быть отрицательным")
+    if r < 0 or not isinstance(r, (int, float)):
+        raise ValueError("Radius must be a non-negative number")
     return 2 * math.pi * r
