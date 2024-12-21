51646f6ef1a106d493c9d5c2e10088c3a629e582
chel1k_hohmas <popilev.sanya@gmail.com>
2024-12-21 14:21:13 +0300

    Update calculate.py

--- calculate.py
+++ calculate.py
@@ -1,54 +1,24 @@
 import circle
 import square
-import triangle
 
-figs = ['circle', 'square', 'triangle']
+
+figs = ['circle', 'square']
 funcs = ['perimeter', 'area']
-sizes = {
-    'circle-area': 1,
-    'circle-perimeter': 1,
-    'square-area': 1,
-    'square-perimeter': 1,
-    'triangle-area': 3,
-    'triangle-perimeter': 3
-}
-
-
+sizes = {}
 def calc(fig, func, size):
-    assert fig in figs, "Invalid figure"
-    assert func in funcs, "Invalid function"
-
-    key = f'{fig}-{func}'
-    args = sizes.get(key)
-    assert args is not None
-    assert len(size) == args
-
-    assert all(s >= 0 for s in size)
-
-    if fig == "triangle":
-        a, b, c = size
-        assert a + b > c and a + c > b and b + c > a
-
-    result = eval(f'{fig}.{func}(*{size})')
-    return result
-
-
+"""
+функция принимает на вход название фигуры, функцию для нее и размеры сторон для фигур, а для круга радиус
+"""
+	assert fig in figs
+	assert func in funcs
+	result = eval(f'{fig}.{func}(*{size})')
+	print(f'{func} of {fig} is {result}')
 if __name__ == "__main__":
-    func = ''
-    fig = ''
-    size = []
-
-    while fig not in figs:
-        fig = input(f"Enter figure name, available are {figs}:\n")
-
-    while func not in funcs:
-        func = input(f"Enter function name, available are {funcs}:\n")
-
-    while len(size) != sizes.get(f"{fig}-{func}", 1):
-        size = list(map(int, input(
-            "Input figure sizes separated by space, 1 for circle and square:\n"
-        ).split()))
-
+	func = ''
+	fig = ''
+	size = list()
     
-    result = calc(fig, func, size)
-    print(f"Result: {result}")
+	while fig not in figs:
+		fig = input(f"Enter figure name, avaliable are {figs}:\n")
+	
+	while func not in funcs:
