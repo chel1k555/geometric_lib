1a3e3ce6e333e473fa77c8d2096d883ea580c52b
chel1k_hohmas <popilev.sanya@gmail.com>
2024-12-21 13:23:50 +0300

    Update test_triangle.py

--- all_tests/test_triangle.py
+++ all_tests/test_triangle.py
@@ -1,37 +1,64 @@
 import unittest
-from triangle import area, perimeter
+import triangle
+from scenarios import scenarios
 
 
-class TestTriangle(unittest.TestCase):
+class TestTriangleArea(unittest.TestCase):
     def test_area(self):
-        x, y, z = 5, 12, 13
-        res = area(x, y, z)
-        self.assertEqual(res, 30)
+        for sides, expected in scenarios['triangle']['area']:
+            with self.subTest(sides=sides, expected=expected):
+                self.assertAlmostEqual(triangle.area(*sides), expected)
 
-    def test_area_zero(self):
-        x, y, z = 0, 0, 0
-        res = area(x, y, z)
-        self.assertEqual(res, 0)
+    def test_area_invalid_negative(self):
+        invalid_scenarios = [
+            (-1, 2, 3), (1, -2, 3), (1, 2, -3)
+        ]
+        for sides in invalid_scenarios:
+            with self.subTest(sides=sides):
+                with self.assertRaises(ValueError):
+                    triangle.area(*sides)
 
+    def test_area_invalid_non_numeric(self):
+        invalid_scenarios = [
+            (1, 'a', 3), ('a', 2, 3), (1, 2, None)
+        ]
+        for sides in invalid_scenarios:
+            with self.subTest(sides=sides):
+                with self.assertRaises(TypeError):
+                    triangle.area(*sides)
+
+    def test_area_invalid_triangle(self):
+        with self.assertRaises(ValueError):
+            triangle.area(1, 1, 10)
+
+
+class TestTrianglePerimeter(unittest.TestCase):
     def test_perimeter(self):
-        x, y, z = 5, 12, 13
-        res = perimeter(x, y, z)
-        self.assertEqual(res, 30)
+        for sides, expected in scenarios['triangle']['perimeter']:
+            with self.subTest(sides=sides, expected=expected):
+                self.assertEqual(triangle.perimeter(*sides), expected)
 
-    def test_perimeter_zero(self):
-        x, y, z = 0, 0, 0
-        res = perimeter(x, y, z)
-        self.assertEqual(res, 0)
+    def test_perimeter_invalid_negative(self):
+        invalid_scenarios = [
+            (-1, 2, 3), (1, -2, 3), (1, 2, -3)
+        ]
+        for sides in invalid_scenarios:
+            with self.subTest(sides=sides):
+                with self.assertRaises(ValueError):
+                    triangle.perimeter(*sides)
 
-    def test_area_neg(self):
-        x, y, z = -5, -12, -13
-        with self.assertRaises(AssertionError):
-            area(x, y, z)
+    def test_perimeter_invalid_non_numeric(self):
+        invalid_scenarios = [
+            (1, 'a', 3), ('a', 2, 3), (1, 2, None)
+        ]
+        for sides in invalid_scenarios:
+            with self.subTest(sides=sides):
+                with self.assertRaises(TypeError):
+                    triangle.perimeter(*sides)
 
-    def test_perimeter_neg(self):
-        x, y, z = -5, -12, -13
-        with self.assertRaises(AssertionError):
-            perimeter(x, y, z)
+    def test_perimeter_invalid_triangle(self):
+        with self.assertRaises(ValueError):
+            triangle.perimeter(1, 1, 10)
 
 
 if __name__ == '__main__':
